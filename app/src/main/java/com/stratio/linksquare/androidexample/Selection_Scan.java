c0c211b8ab6ea472cc6779aaa075da043a99bd88
Brandon Shaver <bnn678@users.noreply.github.com>
2020-03-21 11:00:38 -0500

    Modified Scan Selection and Scan Graph pages
    Revised Selection_Scan so that only individual scans appear rather than individual frames. Modified View_ScanGraph so that the graph displays a line for each frame.

--- app/src/main/java/com/stratio/linksquare/androidexample/Selection_Scan.java
+++ app/src/main/java/com/stratio/linksquare/androidexample/Selection_Scan.java
@@ -3,6 +3,7 @@ package com.stratio.linksquare.androidexample;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,12 +19,9 @@ import java.util.ArrayList;
 public class Selection_Scan extends AppCompatActivity {
     // DECLARE DISPLAY OBJECTS
     Button button_moveBack;
-    Button button_viewScan;
     ListView listView_items;
 
     // DECLARE GLOBALS
-    ArrayList<String> arrayList;
-    ArrayAdapter arrayAdapter;
     DatabaseManager myDb;
 
     @Override
@@ -33,20 +31,14 @@ public class Selection_Scan extends AppCompatActivity {
 
         // INIT DISPLAY OBJECTS
         button_moveBack = findViewById(R.id.button_moveBack);
-        button_viewScan = findViewById(R.id.button_viewScan); // currently an empty object
         listView_items = findViewById(R.id.listView_items);
 
         // INIT GLOBALS
-        arrayList = new ArrayList<String>();
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
         myDb = new DatabaseManager(this);
 
         // CONFIGURE BUTTONS
         configure_button_moveBack();
-        //configure_button_viewScan();
         configure_listView_items();
-
-
     }
 
     private void configure_button_moveBack() {
@@ -58,33 +50,34 @@ public class Selection_Scan extends AppCompatActivity {
         });
     }
 
-    private void configure_button_viewScan() {
-        button_viewScan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Selection_Scan.this, View_ScanGraph.class);
-                startActivity(i);
-            }
-        });
-    }
     private void configure_listView_items() {
         Cursor data = myDb.getData();
-        ArrayList<String> listData = new ArrayList<>();
+        final ArrayList<String> listData = new ArrayList<>();
+
+        StringBuilder s = new StringBuilder("testy1_asdfisf_asdfoijs_Frame6");
+        Log.d("DEBUG", Integer.toString(s.lastIndexOf("_")));
+        Log.d("DEBUG", s.substring(0,s.lastIndexOf("_")));
+
+        String lastScanName = "";
+        String nextScanName;
+
         while (data.moveToNext()) {
-            listData.add(data.getString(6));
-            arrayList.add(data.getString(6));
+            nextScanName = data.getString(6).substring(0, data.getString(6).lastIndexOf("_"));
+
+            if (! nextScanName.equals(lastScanName)) {
+                listData.add(nextScanName);
+                lastScanName = nextScanName;
+            }
         }
+
         ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
         listView_items.setAdapter(adapter);
 
         listView_items.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(Selection_Scan.this,"clicked item: "+i+" "+arrayList.get(i),Toast.LENGTH_SHORT).show();
-                // studyNameText.setText( "Study Name: " + arrayList.get(i).toString() );
-
                 Intent intent = new Intent(getBaseContext(), View_ScanGraph.class);
-                intent.putExtra("localScanID", arrayList.get(i));
+                intent.putExtra("localScanID", listData.get(i));
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/stratio/linksquare/androidexample/View_ScanGraph.java
+++ app/src/main/java/com/stratio/linksquare/androidexample/View_ScanGraph.java
@@ -1,6 +1,7 @@
 package com.stratio.linksquare.androidexample;
 
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -29,17 +30,30 @@ public class View_ScanGraph extends AppCompatActivity {
         myDb = new DatabaseManager(this);
 
         String localScanID = getIntent().getStringExtra("localScanID");
-        Log.d("Debug", localScanID);
+        Log.d("DEBUG", localScanID);
 
-        Cursor spectralValues = myDb.getSpectralValues(localScanID);
-        spectralValues.moveToNext();
-        Log.d("Debug", spectralValues.getString(0));
-        String[] data = spectralValues.getString(0).split(" ");
-        DataPoint[] dataPoints = new DataPoint[data.length];
-        for (int i = 0; i < data.length; i++) {
-            dataPoints[i] = new DataPoint(i, Math.round(Float.parseFloat(data[i])));
+        int frame = 1;
+        while (true) {
+            Cursor spectralValues = myDb.getSpectralValues(localScanID + "_Frame" + frame);
+
+            Log.d("DEBUG", localScanID + "_Frame" + Integer.toString(frame));
+
+            if (spectralValues.moveToFirst()) {
+                String[] data = spectralValues.getString(0).split(" ");
+                DataPoint[] dataPoints = new DataPoint[data.length];
+                for (int i = 0; i < data.length; i++) {
+                    dataPoints[i] = new DataPoint(i, Math.round(Float.parseFloat(data[i])));
+                }
+                LineGraphSeries<DataPoint> plot = new LineGraphSeries<>(dataPoints);
+                // TODO: make each color more distinct
+                // TODO: ensure that color values <= 255
+                plot.setColor(Color.rgb(12*frame, 24*frame, 36*frame));
+                graph.addSeries(plot);
+
+                frame++;
+            } else {
+                break;
+            }
         }
-        LineGraphSeries<DataPoint> plot = new LineGraphSeries<>(dataPoints);
-        graph.addSeries(plot);
     }
 }
