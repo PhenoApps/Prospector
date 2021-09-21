0b178b763cfbe513076b2179deecfedc64e5d048
chaneylc <chaneylc@gmail.com>
2021-09-21 14:49:14 -0500

    Added device press within new sample form to save sample and start new scan. Fixed device not saving data properly.

--- app/src/main/java/org/phenoapps/prospector/fragments/NewSampleFragment.kt
+++ app/src/main/java/org/phenoapps/prospector/fragments/NewSampleFragment.kt
@@ -1,6 +1,7 @@
 package org.phenoapps.prospector.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,11 +20,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import org.phenoapps.prospector.R
+import org.phenoapps.prospector.activities.MainActivity
 import org.phenoapps.prospector.data.models.Sample
 import org.phenoapps.prospector.data.viewmodels.SampleViewModel
 import org.phenoapps.prospector.databinding.FragmentNewSampleBinding
 import org.phenoapps.prospector.utils.Dialogs
 import org.phenoapps.prospector.utils.SnackbarQueue
+import java.lang.IllegalStateException
+import java.util.*
 
 /**
  * A simple data collection fragment that creates sample models and inserts them into the db.
@@ -82,15 +86,16 @@ class NewSampleFragment : Fragment(), CoroutineScope by MainScope() {
 
         mBinding = DataBindingUtil.inflate(localInflater, R.layout.fragment_new_sample, null, true)
 
-        mBinding?.let { _ ->
+        mBinding?.let { binding ->
 
             setupButtons()
 
+            binding.setupToolbar()
+
+            startObservers()
         }
 
         setHasOptionsMenu(true)
-
-//        (activity as? AppCompatActivity)?.findViewById<Toolbar>(R.id.activityToolbar)?.title = ""
 
         return mBinding?.root
     }
@@ -106,7 +111,111 @@ class NewSampleFragment : Fragment(), CoroutineScope by MainScope() {
         }
     }
 
-    private fun FragmentNewSampleBinding.insertSampleAndUpdate() {
+    /**
+     * LinkSquare API live data listener that responds to on-device button clicks.
+     */
+    private fun attachDeviceButtonPressListener() {
+
+        try {
+
+            with (activity as? MainActivity) {
+
+                this?.sDeviceViewModel?.setEventListener {
+
+                    activity?.runOnUiThread {
+
+                        if (sDeviceViewModel.isConnected()) {
+
+                            mBinding?.insertSampleAndUpdate(true)
+
+                        }
+                    }
+
+                }?.observe(viewLifecycleOwner, {})
+            }
+
+        } catch (e: IllegalStateException) {
+
+            Log.d(tag, "Failed to connect LS")
+        }
+    }
+
+    private fun startObservers() {
+
+        //use the activity view model to access the current connection status
+        val check = object : TimerTask() {
+
+            override fun run() {
+
+                activity?.runOnUiThread {
+
+                    with (activity as? MainActivity) {
+                        mBinding?.fragNewSampleToolbar?.menu?.findItem(R.id.action_connection)
+                            ?.setIcon(if (this?.sDeviceViewModel?.isConnected() == true){
+                                attachDeviceButtonPressListener()
+                                R.drawable.ic_vector_link
+                            }
+                            else R.drawable.ic_vector_difference_ab)
+                    }
+                }
+            }
+        }
+
+        Timer().cancel()
+
+        Timer().purge()
+
+        Timer().scheduleAtFixedRate(check, 0, 1500)
+
+        //set the title header
+        sViewModel.experiments.observe(viewLifecycleOwner, { experiments ->
+
+            experiments.first { it.eid == mExpId }.also {
+
+                activity?.runOnUiThread {
+
+                    mBinding?.fragNewSampleToolbar?.title = it.name
+
+                }
+            }
+        })
+
+        attachDeviceButtonPressListener()
+    }
+
+    private fun FragmentNewSampleBinding.setupToolbar() {
+
+        fragNewSampleToolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        fragNewSampleToolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+
+                R.id.action_connection -> {
+
+                    with (activity as? MainActivity) {
+
+                        if (this?.mConnected == true) {
+
+                            sDeviceViewModel.reset()
+                        } else {
+
+                            findNavController().navigate(
+                                NewSampleFragmentDirections
+                                .actionToConnectInstructions())
+
+                            this?.startDeviceConnection()
+                        }
+                    }
+                }
+            }
+
+            true
+        }
+    }
+
+    private fun FragmentNewSampleBinding.insertSampleAndUpdate(startScan: Boolean = false) {
 
         activity?.let { act ->
 
@@ -115,22 +224,26 @@ class NewSampleFragment : Fragment(), CoroutineScope by MainScope() {
 
             val newSampleString: String = act.getString(R.string.dialog_new_samples_prefix)
 
-            launch {
+            if (name.isNotBlank()) {
 
-                sViewModel.insertSampleAsync(Sample(mExpId, name, note = notes)).await()
+                launch {
 
-            }
+                    sViewModel.insertSampleAsync(Sample(mExpId, name, note = notes)).await()
 
-            activity?.runOnUiThread {
+                    act.runOnUiThread {
 
-                mSnackbar.push(SnackbarQueue.SnackJob(root, "$newSampleString $name."))
+                        mSnackbar.push(SnackbarQueue.SnackJob(root, "$newSampleString $name."))
 
-                clearUi()
+                        if (startScan) {
 
-                findNavController().popBackStack()
+                            findNavController().navigate(NewSampleFragmentDirections
+                                .actionToScanList(mExpId, name, startScan))
+
+                        } else findNavController().popBackStack()
+                    }
+                }
             }
         }
-
     }
 
     //checks if sample name is not empty
