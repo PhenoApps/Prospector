51d7d0413198d446832b90245f6b7c9e78eec55f
chaneylc <chaneylc@gmail.com>
2021-09-21 17:10:41 -0500

    Added new date display

--- app/src/main/java/org/phenoapps/prospector/utils/DateUtil.kt
+++ app/src/main/java/org/phenoapps/prospector/utils/DateUtil.kt
@@ -1,6 +1,7 @@
 package org.phenoapps.prospector.utils
 
 import android.os.Build
+import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -13,5 +14,23 @@ class DateUtil {
             "yyyy-MM-dd-hh-mm-ss"))
     } else {
         Calendar.getInstance().time.toString()
+    }
+
+    fun displayTime(date: String): String = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+        try {
+
+            val dbFormat = SimpleDateFormat("yyyy-MM-dd-hh-mm-ss", Locale.ENGLISH)
+            val uiFormat = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
+
+            uiFormat.format(dbFormat.parse(date))
+
+        } catch (e: Exception) {
+
+            ""
+        }
+
+    } else {
+        date
     }
 }
