f4b07d4e2604d37fd3ff4bc8a4954fa431997a68
chaneylc <chaneylc@gmail.com>
2021-09-24 17:09:05 -0500

    added light source to sample file import

--- app/src/main/java/org/phenoapps/prospector/activities/MainActivity.kt
+++ app/src/main/java/org/phenoapps/prospector/activities/MainActivity.kt
@@ -265,7 +265,7 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
                             )
                         ).await()
 
-                        sViewModel.insertFrame(sid, SpectralFrame(sid, 0, waveRange, 0))
+                        sViewModel.insertFrame(sid, SpectralFrame(sid, 0, waveRange, lightSource.toInt()))
 
                     } catch (e: Exception) {
 
